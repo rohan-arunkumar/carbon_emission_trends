07fab08776480a4a59877f7ddbbae93bde869c30
rohan-arunkumar <129446596+rohan-arunkumar@users.noreply.github.com>
2024-12-14 16:50:41 +0000

    added image

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st 
 
+img_url = "https://media.istockphoto.com/id/1254838231/vector/cars-air-pollution-polluted-air-environment-at-city-vehicle-traffic-and-toxic-pollution-car.jpg"
+st.image(img_url, caption="Vehicle Pollution")
 st.title("Carbon Emission Trends")
 st.header("Information")
 st.write("""The largest factors for CO2 emissions:
