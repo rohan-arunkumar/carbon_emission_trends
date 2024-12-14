d9f6fe801b28e80be72ca5961b7da65464bad9c4
rohan-arunkumar <129446596+rohan-arunkumar@users.noreply.github.com>
2024-12-14 18:41:41 +0000

    input buttons

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import streamlit as st 
+import streamlit as st
+import pickle 
 from PIL import Image
+
+# loading in the model to predict on the data
+pickle_in = open('best_model_MLP_subset1.pkl', 'rb')
+classifier = pickle.load(pickle_in)
+
+Fuel_type = st.text_input("Fuel Type", "Type Here")
+Fuel_type = st.text_input("Fuel Type", "Type Here")
+Fuel_type = st.text_input("Fuel Type", "Type Here")
+Fuel_type = st.text_input("Fuel Type", "Type Here")
+Fuel_type = st.text_input("Fuel Type", "Type Here")
+Fuel_type = st.text_input("Fuel Type", "Type Here")
+
+if st.button("Predict"):
+    result = prediction(sepal_length, sepal_width, petal_length, petal_width)
+    st.success('The output is {}'.format(result))
+
 
 img_url = "carbonbanner.jpg"
 image = Image.open(img_url)
