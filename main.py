cc5b46982c74e2cfb0139fecbd41de3c07b53b28
rohan-arunkumar <129446596+rohan-arunkumar@users.noreply.github.com>
2024-12-14 18:55:15 +0000

    input buttons

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from PIL import Image
 pickle_in = open('best_model_MLP_subset1.pkl', 'rb')
 classifier = pickle.load(pickle_in)
 
-Fuel_type = st.text_input("Fuel Type", "Type Here")
-Fuel_type = st.text_input("Fuel Type", "Type Here")
-Fuel_type = st.text_input("Fuel Type", "Type Here")
-Fuel_type = st.text_input("Fuel Type", "Type Here")
-Fuel_type = st.text_input("Fuel Type", "Type Here")
-Fuel_type = st.text_input("Fuel Type", "Type Here")
+fuel_type = st.text_input("Fuel Type", "Type Here")
+engine_size = st.text_input("Engine Size", "Type Here")
+cylinders = st.text_input("Cylinders", "Type Here")
+transmission = st.text_input("Transmission", "Type Here")
+fuel_consumption_city = st.text_input("Fuel Consumption City (L/100 km)", "Type Here")
+fuel_consumption_hwy = st.text_input("Fuel Consumption Hwy (L/100 km)", "Type Here")
+fuel_consumption_combL = st.text_input("Fuel Consumption Comb (L/100 km)", "Type Here")
+fuel_consumption_combG = st.text_input("Fuel Consumption Comb (mpg)", "Type Here")
+
 
 if st.button("Predict"):
-    result = prediction(sepal_length, sepal_width, petal_length, petal_width)
+    result = prediction(fuel_type, engine_size, cylinders, transmission, fuel_consumption_city, fuel_consumption_hwy, fuel_consumption_combL, fuel_consumption_combG)
     st.success('The output is {}'.format(result))
 
 
