ad4984ea6e2e5c19a4169fbbc9fd724567a5114e
rohan-arunkumar <129446596+rohan-arunkumar@users.noreply.github.com>
2024-12-14 16:56:53 +0000

    added image

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st 
+from PIL import Image
 
 img_url = "carbonbanner.jpg"
-st.image(img_url, caption="Vehicle Pollution")
+image = Image.open(image_url)
+st.image(image, caption="Vehicle Pollution")
 st.title("Carbon Emission Trends")
 st.header("Information")
 st.write("""The largest factors for CO2 emissions:
